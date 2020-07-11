7448ae1b0d7d21f41a352ae557982306c94af35e
Yee Hui Poh <yhpoh91@gmail.com>
2020-07-11 20:07:10 +0800

    Add support for logger and error handler

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-
 const router = express.Router({ mergeParams: true });
 
 router.get('/', (_, res) => res.send('You have reached the API Router'));
--- src/web.js
+++ src/web.js
@@ -5,16 +5,20 @@ import http from 'http';
 import helmet from 'helmet';
 
 import apiRouter from './api';
+import loggerService from './services/logger';
+import errorHandler from './services/errorHandler';
 
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV || 'development';
 const listenIp = '0.0.0.0';
 const listenPort = process.env.PORT || 8080;
 const isHeroku = (process.env.IS_HEROKU || 'false').toLowerCase() === 'true';
 const publicHost = isHeroku ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : process.env.PUBLIC_HOST;
 
+const { L } = loggerService('Web Application');
+
 // Always use UTC Timezone
 const timezone = process.env.TIMEZONE || 'Etc/UTC';
-console.log(`Using Timezone: ${timezone}`);
+L.info(`Using Timezone: ${timezone}`);
 process.env.TZ = timezone;
 
 // Application
@@ -28,14 +32,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (_, res) => res.send('You have reached Kopi SIO'));
 
 // Api Router
+app.get('/', (_, res) => res.send('You have reached Kopi SIO'));
 app.use('/api', apiRouter);
+app.use(errorHandler.handleUnmatched);
+app.use(errorHandler.handleError);
 
 // Server
 const httpServer = http.createServer(app);
 httpServer.listen(listenPort, listenIp, () => {
-  console.log(`Server (${environment}) listening on ${listenIp}:${listenPort}`)
-  console.log(`Server is now accessible at ${publicHost}`)
+  L.info(`Server (${environment}) listening on ${listenIp}:${listenPort}`);
+  L.info(`Server is now accessible at ${publicHost}`);
 });
