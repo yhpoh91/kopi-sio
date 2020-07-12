b2c9a153f06564992c4a48f74b86104fa9f14020
Yee Hui Poh <yhpoh91@gmail.com>
2020-07-12 12:46:42 +0800

    add getting token support for facebook oauth

--- src/services/oauth/facebook.js
+++ src/services/oauth/facebook.js
@@ -7,13 +7,29 @@ const getOAuthUrl = () => {
   const state = 'teststate';
   const redirectUri = `${publicHost}/oauth/facebook/redirect`;
   const clientId = process.env.OAUTH_FACEBOOK_CLIENT_ID;
-  return `https://www.facebook.com/v7.0/dialog/oauth?state=${state}&redirect_uri=${redirectUri}&client_id=${clientId}`;
+  const scope = (process.env.OAUTH_FACEBOOK_SCOPE || '').replace(/,/g, ' ');
+  const rerequest = (process.env.OAUTH_FACEBOOK_REREQUEST || 'false').toLowerCase() === 'true';
+
+  let url = `https://www.facebook.com/v7.0/dialog/oauth?state=${state}&redirect_uri=${redirectUri}&client_id=${clientId}&scope=${scope}`;
+
+  if (rerequest) {
+    url += `&auth_type=rerequest`;
+  }
+
+  return url;
 };
 
 const getToken = async (code) => {
   const clientId = process.env.OAUTH_FACEBOOK_CLIENT_ID;
   const clientSecret = process.env.OAUTH_FACEBOOK_CLIENT_SECRET;
-  return Promise.reject(new Error('not implemented yet'));
+  const redirectUri = `${publicHost}/oauth/facebook/redirect`;
+
+  const url = `https://graph.facebook.com/v7.0/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&code=${code}`;
+  const response = await axios.get(url);
+  const { data } = response;
+
+  console.log(data);
+  return Promise.resolve(data);
 };
 
 const deleteData = async () => {
